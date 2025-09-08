962729fc35c28823b9738c0e3efa9b9a9731e816
carlDguez19 <110745188+carlDguez19@users.noreply.github.com>
2025-09-08 14:33:45 -0600

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  {
+    ignores: ["webpack.config.js"],
+  },
+  eslintConfigPrettier,
+];
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { hello } from "./tester";
+import "./styles.css";
+hello();
